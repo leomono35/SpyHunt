10eb5f815eae1a831a6865c2bfd05e8d97936e77
Leo Mallard <leo.mallard@utbm.fr>
2020-05-05 23:32:53 +0200

    first commit

---
+++ Components/Rules.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {View, Text, Button, StyleSheet} from 'react-native'
+
+class Rules extends React.Component{
+
+  constructor() {
+    super()
+    this.state = {
+      showRule: false
+    }
+  }
+
+  _showRules = ()  => {
+    this.setState({showRule: true})
+  }
+
+  render() {
+    return (
+        <View style = {styles.viewContainer}>
+          {this.state.showRule ? <Text style= {{ fontSize: 75, color: "#000", textAlign: 'center' }}> Hello Friends </Text> : null}
+          {!this.state.showRule ? <Button title='Une première fois ? Apprends les règles !' onPress={this._showRules}/> : null}
+        </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'cyan'
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default Rules
---
+++ Navigation/TabNavigation.js
@@ -0,0 +1,4 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as React from 'react';
+
+const BottomTab = createBottomTabNavigator();
